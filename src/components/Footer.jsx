046434a7407b21d97927ce50ca63643cf552e69f
NathalieBaskin <nathalie.baskin@utb.ecutbildning.se>
2025-02-13 11:45:04 +0100

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <h3 className="footer-title">NABASKI</h3>
+      <ul className="footer-links">
+        <li><a href="#">Prislita</a></li>
+        <li><a href="#">Kundrecensioner</a></li>
+        <li><a href="#">Kundgalleri</a></li>
+        <li><a href="#">FAQ</a></li>
+      </ul>
+      <div className="footer-icons">
+        <a href="#"><i className="fas fa-envelope"></i></a>
+        <a href="#"><i className="fab fa-facebook"></i></a>
+        <a href="#"><i className="fab fa-instagram"></i></a>
+        <a href="#"><i className="fab fa-youtube"></i></a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
