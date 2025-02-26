5125d520972868679fd3443f4ffb6a408c79c014
NathalieBaskin <nathalie.baskin@utb.ecutbildning.se>
2025-02-26 12:31:43 +0100

    css för sök och checkout

--- src/pages/Sok.jsx
+++ src/pages/Sok.jsx
@@ -1,6 +1,6 @@
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-
+import "./Sok.css";
 function Sok() {
   const location = useLocation();
   const navigate = useNavigate();
