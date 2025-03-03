742a12fd9e958119a8ba30008fe7ab0cce7da631
NathalieBaskin <nathalie.baskin@utb.ecutbildning.se>
2025-03-03 13:18:22 +0100

    copyright symbol i footern

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="footer">
       <h3 className="footer-title">NABASKI</h3>
-      <p className="footer-name">Nathalie Baskin</p>
+      <p className="footer-name">Nathalie Baskin ©</p>
       <ul className="footer-links"></ul>
       <div className="footer-icons">
         <a href="https://www.facebook.com/namahka"><i className="fab fa-facebook"></i></a>
