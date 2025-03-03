22518b8643ac662369d48ec1dfdff97d4ac7a452
NathalieBaskin <nathalie.baskin@utb.ecutbildning.se>
2025-03-03 12:29:34 +0100

    footer placerad längst ner

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,14 +5,10 @@ function Footer() {
     <footer className="footer">
       <h3 className="footer-title">NABASKI</h3>
       <p className="footer-name">Nathalie Baskin</p>
-      <ul className="footer-links">
-   
-      </ul>
+      <ul className="footer-links"></ul>
       <div className="footer-icons">
-   
         <a href="https://www.facebook.com/namahka"><i className="fab fa-facebook"></i></a>
         <a href="https://www.instagram.com/namahka/"><i className="fab fa-instagram"></i></a>
-
       </div>
     </footer>
   );
