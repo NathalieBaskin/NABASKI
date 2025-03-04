b3ccece7af6dbec1004d354dc72412aec51e8ae2
NathalieBaskin <nathalie.baskin@utb.ecutbildning.se>
2025-03-04 11:06:58 +0100

    ändrar videon till en videokarusell

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,12 +1,29 @@
 import "./Video.css";
 
+const videos = [
+  "/videos/video1.mp4",
+  "/videos/video2.mp4",
+  "/videos/video3.mp4",
+  "/videos/video4.mp4",
+  "/videos/video5.mp4",
+  "/videos/video6.mp4",
+  "/videos/video7.mp4",
+  "/videos/video8.mp4",
+  "/videos/video9.mp4",
+  "/videos/video10.mp4",
+  "/videos/video11.mp4",
+  "/videos/video12.mp4",
+  "/videos/video13.mp4",
+];
+
 function Video() {
   return (
     <div className="video-container">
-      <video autoPlay loop muted className="background-video">
-        <source src="/NABASKI.mp4" type="video/mp4" />
-        Din webbläsare stödjer inte videon.
-      </video>
+      <div className="video-carousel">
+        {[...videos, ...videos].map((video, index) => (
+          <video key={index} src={video} autoPlay loop muted className="background-video"></video>
+        ))}
+      </div>
     </div>
   );
 }
